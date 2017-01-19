e6b2c3f013b75f19f961fe09eb86c826264174db
hisaruki <hisaruki@morningnegi.ddo.jp>
2017-01-19 23:01:53 +0900

    ログイン後の保存は不可能なことがわかったので諦める。既定のドライバをPhantomJSに変更。

--- playbooks.py
+++ playbooks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import logging,time,re,requests,mimetypes,argparse,os,urllib,base64
+import logging,time,re,requests,mimetypes,argparse,os
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,30 +17,26 @@ class PlayBooks:
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     ch.setFormatter(formatter)
-    self.logger.addHandler(ch)
     #fh = logging.FileHandler("playbooks.log",mode="a")
     #fh.setLevel(logging.INFO)
     #fh.setFormatter(formatter)
+    self.logger.addHandler(ch)
     #self.logger.addHandler(fh)
     self.logger.debug("Initialize..")
-    self.images = {}
 
-  def login(self,username,password):
+  def login(self):
     self.logger.debug("Logging in...")
     self.driver.get('https://accounts.google.com/')
     WebDriverWait(self.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR,'#Email'))
     )
-    while not username:
-      username = input("username:")
-    self.driver.find_elements_by_css_selector('#Email')[0].send_keys(username)
+
+    self.driver.find_elements_by_css_selector('#Email')[0].send_keys(input("username:"))
     self.driver.find_elements_by_css_selector('#next')[0].send_keys('\n')
     WebDriverWait(self.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR,'#Passwd'))
     )
-    while not password:
-      password = input("password:")
-    self.driver.find_elements_by_css_selector('#Passwd')[0].send_keys(password)
+    self.driver.find_elements_by_css_selector('#Passwd')[0].send_keys(input("password:"))
     self.driver.find_elements_by_css_selector('#signIn')[0].send_keys('\n')
     WebDriverWait(self.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR,'body'))
@@ -58,8 +54,8 @@ class PlayBooks:
         service_log_path=os.path.devnull
       )
 
-  def fetch(self,url):
-    self.logger.debug("Getting Images...")
+  def list(self,url):
+    self.logger.debug("Getting Images List...")
     self.driver.get(url)
     WebDriverWait(self.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR,'[id=":0.reader"]'))
@@ -70,47 +66,15 @@ class PlayBooks:
       EC.presence_of_element_located((By.CSS_SELECTOR,'body[class]'))
     )
 
-    time.sleep(2)
-    document = BeautifulSoup(self.driver.page_source,"lxml")
-    self.title = document.select("body > div > div > table td > span > span")[0].get_text()
-    try:
-      Path(self.title).mkdir()
-    except:
-      ""
+    result = []
 
     def page(key):
       document = BeautifulSoup(self.driver.page_source,"lxml")
       b = document.select("body > div > div > table td > div > div > div")[-1]
-      images = self.driver.execute_script('''
-function ImageToBase64(img,mimetype) {
-  var canvas = document.createElement('canvas');
-  canvas.width  = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-  return canvas.toDataURL(mimetype);
-}
-var images = [];
-document.querySelectorAll("img").forEach(function(img){
-  img.src = img.src.replace(/&w=.*/,"")
-  try{
-    var b64 = ImageToBase64(img,"image/jpeg");
-    images.push([img.src,b64,".jpg"]);
-  }catch(e){}
-  try{
-    var b64 = ImageToBase64(img,"image/png");
-    images.push([img.src,b64,".png"]);
-  }catch(e){}
-});
-return images;
-      ''');
-      for image in images:
-        url,b64,ext = image[0],image[1],image[2]
-        b64 = re.sub(r'data:[^,]*,','',b64)
-        if len(b64) and not url in self.images:
-          self.images[url] = b64,ext
-        if url in self.images and len(b64) > len(self.images[url][0]):
-          self.images[url] = b64,ext
+      for img in document.select("img"):
+        img = re.sub('&w=[0-9]*','',img.get("src"))
+        if not img in result:
+          result.append(img)
       self.logger.debug({
         u'\ue00e':"Pageup.",
         u'\ue00f':"Pagedown."
@@ -121,38 +85,55 @@ return images;
       time.sleep(0.1)
       document = BeautifulSoup(self.driver.page_source,"lxml")
       a = document.select("body > div > div > table td > div > div > div")[-1]
+      for img in document.select("img"):
+        img = re.sub('&w=[0-9]*','',img.get("src"))
+        if not img in result:
+          result.append(img)
       return int(b == a)
 
+    time.sleep(2)
+
+    document = BeautifulSoup(self.driver.page_source,"lxml")
+    self.title = document.select("body > div > div > table td > span > span")[0].get_text()
     up = u'\ue00e'
     down = u'\ue00f'
+
     uperr = 0
     downerr = 0
+
     while uperr < 5:
       uperr += page(up)
-    while downerr < 5:
+
+    while downerr < 10:
       downerr += page(down)
     self.driver.quit()
 
-  def save(self):
-    for url in self.images:
-      b64 = self.images[url][0]
-      ext = self.images[url][1]
-      qs = urllib.parse.parse_qs(url)
-      if "pg" in qs:
-        pg = qs["pg"][0]
-        with (Path(self.title) / Path(pg+ext)).open("wb") as f:
-          f.write(base64.b64decode(b64))
+    return list(set(result))
+
+  def save(self,imgurl):
+    try:
+      Path(self.title).mkdir()
+    except:
+      ""
+    n = self.title+"/"+re.search('pg=[^&]*',imgurl).group(0).replace("pg=","")
+    ext = ".jpg"
+    self.logger.info(imgurl)
+    r = requests.get(imgurl)
+    ext = mimetypes.guess_extension(r.headers["content-type"], strict=False)
+    if ext == ".jpe" or ext == ".jpeg":
+      ext = ".jpg"
+    with Path(n+ext).open("wb") as f:
+      f.write(r.content)
 
 parser = argparse.ArgumentParser(description="playbooks")
 parser.add_argument("url")
-parser.add_argument("--login",action="store_true")
-parser.add_argument("-u","--username",default=None)
-parser.add_argument("-p","--password",default=None)
+parser.add_argument("--driver",default="PhantomJS")
 args = parser.parse_args()
 
 p = PlayBooks()
-p.boot(driver="Chrome")
-if args.login:p.login(args.username,args.password)
+p.boot(args.driver)
+#if args.login:p.login()
 url = args.url
-p.fetch(url)
-p.save()
+for imgurl in p.list(url):
+  p.save(imgurl)
+
